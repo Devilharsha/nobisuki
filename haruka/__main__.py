77db9324bc8a5186aa2887d5917076caa5856ff0
GodOfanjal <95070448+GodOfanjal@users.noreply.github.com>
2021-12-14 06:43:16 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -153,15 +153,26 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START
 
-    keyboard = [[InlineKeyboardButton(text="🇮🇳 Language 🇮🇳", callback_data="set_lang_")]]
-    keyboard += [[InlineKeyboardButton(text="⚜ Reporting ⚜", callback_data="cntrl_panel_M"), 
-        InlineKeyboardButton(text="❓ Help ❓", callback_data="help_back")],
-                 [
+    buttons = [
+    [
         InlineKeyboardButton(
-            text="🤗 Add Me 🥳", url="http://t.me/SoulQueenBot?startgroup=true"
-        ),
+                            text="☑ Add Vegeta to Group ☑",
+                            url="t.me/VegetaRobot?startgroup=true"),
+                    ],
+                   [
+                       InlineKeyboardButton(text="✧ Network", url="t.me/pegasusXteam"),
+                       InlineKeyboardButton(text="✧ Logs", url="t.me/pegasusLogs"),
+                     ],
+                    [                  
+                       InlineKeyboardButton(
+                             text="✧ Support",
+                             url=f"https://t.me/UnitedSupport"),
+                       InlineKeyboardButton(
+                             text="✧ Updates",
+                             url=f"https://t.me/PegasusUpdates"
+         ),
     ],
-]
+] 
 
     update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
